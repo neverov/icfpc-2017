1a949d63c5ecf9131d9524aad3b615f7ce9f62d5
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-08-07 12:53:25 +0300

    added proper tcp/read-msg function

--- src/punter/tcp.clj
+++ src/punter/tcp.clj
@@ -1,7 +1,8 @@
 (ns punter.tcp
   (:gen-class)
   (:require [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [punter.util :refer [log]])
   (:import (java.net Socket)))
 
 (defn connect [in-stream out-stream]
@@ -14,13 +15,38 @@
         in (.getInputStream socket)
         out (.getOutputStream socket)]
     (.setSoTimeout socket 10000) ; wait 10 sec for the game to start
-    (connect in out)))        
+    (connect in out)))
 
 (defn write [conn msg]
   (println "sending message:" msg)
   (doto (:out conn)
     (.write msg 0 (count msg))
     (.flush)))
+
+(defn- read-until
+  [input-stream stop-symbol]
+  (loop [string ""]
+    (let [c (-> input-stream .read char)]
+      (if (= c stop-symbol)
+        string
+        (recur (str string c))))))
+
+(defn- read-symbols
+  [input-stream amount]
+  (loop [length 0
+         string ""]
+    (if (< length amount)
+      (recur (+ 1 length)
+             (str string (-> input-stream .read char)))
+      string)))
+
+(defn read-msg
+  [input-stream]
+  (let [length (-> (read-until input-stream \:)
+                   read-string)
+        msg (read-symbols input-stream length)]
+    (log "tcp/read-msg:" msg)
+    msg))
 
 (defn read-line [conn]
   (.readLine (:in conn)))
